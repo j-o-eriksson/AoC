c6fe79b196d28f8fe3a23fda14c305ff513f2e91
Jonatan Eriksson <jonataer@lap5cg1111r18.se.axis.com>
2023-12-13 16:21:32 +0100

    13 a

--- 2023/day13/day13.py
+++ 2023/day13/day13.py
@@ -9,18 +9,24 @@ def parse(pattern: str):
 
 
 def find_reflection_line(lines):
-    return [i for i in range(len(lines) - 1) if lines[i] == lines[i + 1]]
+    candidates = [i for i in range(len(lines) - 1) if lines[i] == lines[i + 1]]
+    return [c for c in candidates if is_valid(lines, c)]
 
 
 def is_valid(lines, i):
-    n = len(lines)
-    return all(lines[i - j] != lines[i + j + 1] for j in range(min(i, n - i)))
+    n = min(i + 1, len(lines) - i - 1)  # 01234 5678
+    return all(lines[i - j] == lines[i + j + 1] for j in range(n))
 
 
 patterns = [parse(p) for p in open(sys.argv[1]).read().split("\n\n")]
 
 # part 1
+count = 0
 for rp, cp in patterns:
-    print("row:", find_reflection_line(rp))
-    print("col:", find_reflection_line(cp))
-    print()
+    a = find_reflection_line(rp)
+    if a != []:
+        count += 100 * (a[0] + 1)
+    else:
+        b = find_reflection_line(cp)
+        count += b[0] + 1
+print(count)
