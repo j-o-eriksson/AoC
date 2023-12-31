da9d3e731403d56cb2e513f8bb0ffe260ab094b2
jon <jonatan.o.eriksson@gmail.com>
2023-12-31 12:21:47 +0100

    2023 day 17 part 2

--- 2023/day17/day17.py
+++ 2023/day17/day17.py
@@ -16,7 +16,7 @@ class Data(typing.NamedTuple):
     cost: int
 
 
-def _directions(d: Data) -> typing.List[typing.Tuple[typing.Tuple[int, int], int, int]]:
+def directions1(d: Data) -> typing.List[typing.Tuple[typing.Tuple[int, int], int, int]]:
     ds = [(0, -1), (-1, 0), (0, 1), (1, 0)]  # LTRB
     ii = [(d.dir - 1) % 4, (d.dir + 1) % 4]
     if d.steps == 3:
@@ -25,10 +25,21 @@ def _directions(d: Data) -> typing.List[typing.Tuple[typing.Tuple[int, int], int
         return [(ds[i], i, 1) for i in ii] + [(ds[d.dir], d.dir, d.steps + 1)]
 
 
+def directions2(d: Data) -> typing.List[typing.Tuple[typing.Tuple[int, int], int, int]]:
+    ds = [(0, -1), (-1, 0), (0, 1), (1, 0)]  # LTRB
+    ii = [(d.dir - 1) % 4, (d.dir + 1) % 4]
+    if d.steps < 4:
+        return [(ds[d.dir], d.dir, d.steps + 1)]
+    elif 4 <= d.steps < 10:
+        return [(ds[i], i, 1) for i in ii] + [(ds[d.dir], d.dir, d.steps + 1)]
+    else:
+        return [(ds[i], i, 1) for i in ii]
+
+
 def candidates(l: Data, visited) -> typing.List[Data]:
     return [
         Data(l.r + dr, l.c + dc, dir, steps, l.cost + g[l.r + dr][l.c + dc])
-        for (dr, dc), dir, steps in _directions(l)
+        for (dr, dc), dir, steps in directions2(l)
         if (l.r + dr, l.c + dc, dir, steps) not in visited
         and 0 <= l.r + dr < rmax
         and 0 <= l.c + dc < cmax
@@ -47,17 +58,13 @@ def explore(start, end):
     visited = set()
     q = [start]
 
-    count = 0
     while True:
-        count += 1
         d = pop(q, visited)
         q += candidates(d, visited)
         q.sort(key=lambda x: -x.cost)
 
         if (d.r, d.c) == end:
-            print("count:", count, rmax * cmax)
             return d.cost
 
 
-r0, c0 = 0, 0
-print(explore(Data(r0, c0, dir=2, steps=0, cost=0), (rmax - 1, cmax - 1)))
+print(explore(Data(r=0, c=0, dir=2, steps=0, cost=0), (rmax - 1, cmax - 1)))
